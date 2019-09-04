7cf63c5f0733a33f570f51078f7b83702f0870d3
Tomodi <sechaba.tomodi@accenture.com>
2019-09-04 14:37:56 +0200

    Updates

---
+++ precache-manifest.9b42dc09f6b9362db417a722435efd34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffb3719fa55286d70fdfc9d517aff46",
+    "url": "/sechaba-miniApp/index.html"
+  },
+  {
+    "revision": "9ece344f93a30447341c",
+    "url": "/sechaba-miniApp/static/css/main.a6741374.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/sechaba-miniApp/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "9ece344f93a30447341c",
+    "url": "/sechaba-miniApp/static/js/main.53503f10.chunk.js"
+  },
+  {
+    "revision": "12630670e8e0d6cd22f0",
+    "url": "/sechaba-miniApp/static/js/runtime~main.072e2cc6.js"
+  }
+]);
